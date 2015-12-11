bec30cd2b2d650fbb5ee9a2fa1e0e080ea99dc5b
Joseph Chingalo <profschingalo@gmail.com>
2015-12-11 12:08:09 +0300

    testing select option

--- www/app/app.js
+++ www/app/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by joseph on 10/21/15.
  */
-var app = angular.module('roadsafetyMobileApp', ['ui.date','ngRoute','ngMaterial','ngStorage'])
+var app = angular.module('roadsafetyMobileApp', ['ui.materialize','ngRoute','ngMaterial','ngStorage'])
     .config(function($routeProvider){
     $routeProvider
         .when('/reload', {
