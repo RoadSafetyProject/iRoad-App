7f31cd30896fc7f38ac6ab0dfba6bb0e13f7ffe3
Joseph Chingalo <profschingalo@gmail.com>
2015-12-18 10:47:26 +0300

    testing upload signature file

--- www/app/controllers/reportAccidentsController.js
+++ www/app/controllers/reportAccidentsController.js
@@ -344,7 +344,7 @@ app.controller('reportAccidentsController',function($scope,$rootScope,$localStor
         ft.upload($scope.policeSignature, encodeURI($localStorage.url + "/api/fileResources"), function(result) {
 
                 Materialize.toast('Success upload police data',3000);
-                alert('response : ' + result.response);
+                alert('response : ' + result.response.fileResource.id);
             },
             function(error) {
                 alert('error : ' + JSON.stringify(error));
